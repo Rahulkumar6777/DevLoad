47a7c9bab3159a9c7f3a2e86ea0b053ca3da5971
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-27 19:36:45 +0530

    add important routes on app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,11 @@ import Navbar from "./components/Navbar";
 import { Home } from "./pages/Home";
 import navbarRoutes from "./components/NavbarRoutes";
 import ScrollToTop from "./components/ScrollToTop";
+import Price from "./pages/Price";
+import About from "./pages/About";
+import DevelopersSection from "./pages/Developers";
+import Documentation from "./pages/Documentation";
+import TermsOfService from "./pages/Term";
 
 function App() {
   const location = useLocation();
@@ -17,7 +22,12 @@ function App() {
       {showNavbar && <Navbar />}
       <div>
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/pricing" element={<Price />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/developers" element={<DevelopersSection />} />
+          <Route path="/documentation" element={<Documentation />} />
+          <Route path="/term" element={<TermsOfService />} />
         </Routes>
       </div>
     </div>
@@ -26,7 +36,7 @@ function App() {
 
 const AppWrapper = () => (
   <Router>
-    <ScrollToTop/>
+    <ScrollToTop />
     <App />
   </Router>
 );
