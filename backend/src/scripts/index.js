91bc98817a39ce5575381b5adee3805b327615a9
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-28 14:12:36 +0530

    remove import path

--- backend/src/scripts/index.js
+++ backend/src/scripts/index.js
@@ -1,9 +1,5 @@
-import runMiniOContainers from "./slice/minioDockerForLocal.js";
-import runRedisContainers from "./slice/redisDockerForLocal.js";
 import runContainers from "./slice/serviceStart.js";
 
 export const runcontainer = async () => {
-    // await runRedisContainers();
-    // await runMiniOContainers()
     await runContainers()
 } 
