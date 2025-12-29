a1335aa4d096874cc52dc35b2fefa64fb32091f9
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-30 01:31:53 +0530

    update

--- worker/src/config/s3client.js
+++ worker/src/config/s3client.js
@@ -3,7 +3,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 export const s3client = (endpoint, accessId, accessKey) => {
   return new S3Client({
     endpoint: endpoint,
-    region: 'auto',
+    region: 'us-east-1',
     forcePathStyle: true,
     credentials: {
       accessKeyId: accessId,
