3f3030f615d0c1999f2d32f6e49c9dc77a0ea183
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-31 23:04:51 +0530

    make verifyjwt middleware

---
+++ backend/src/middleware/Auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import { User } from '../Models/user.Model.js';
+
+const verifyJWT = async (req, res, next) => {
+    try {
+        const accessToken = req.headers['authorization']?.split(' ')[1] || req.cookies.AccessToken;
+        if (!accessToken) {
+            return res.status(401).json({ message: "Unauthorized access" });
+        }
+
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded._id;
+
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(403).json({ message: "Invalid User" });
+        }
+
+        if (user.status === 'banned') {
+            return res.status(403).json({
+                message: "Your Account temporarly Banned"
+            })
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(500).json({ error: "Please relogin" });
+    }
+};
+
+
+export { verifyJWT };
