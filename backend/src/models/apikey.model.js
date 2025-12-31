4e190b650669f57a0fea4edc211a5e17b8dcba42
Rahulkumar6777 <rahulk48546@gmail.com>
2025-12-31 18:17:03 +0530

    make api key model

---
+++ backend/src/models/apikey.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+
+const apikeyschema = new mongoose.Schema({
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    projectid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    key: {
+        type: String,
+        required: true,
+        index: true,
+        unique: true
+    },
+    status: {
+        type: String,
+        enum: ['active', 'revoked'],
+        default: 'active',
+    },
+}, {
+    timestamps: true
+})
+
+
+apikeyschema.index({ key: 1, projectid: 1 });
+
+
+const Apikey = mongoose.model('Apikey', apikeyschema);
+
+export { Apikey };
