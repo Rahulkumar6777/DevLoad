e3e1bc72629c23de1a4c665ab6708ec73513b078
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-01 12:44:02 +0530

    add project routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,18 +1,21 @@
 import express from "express"
 import { Auth } from "../controllers/Auth/index.js";
 import { verifyJWT } from "../middleware/Auth.js";
-import { Project} from '../controllers/Project/index.js'
+import { Project } from '../controllers/Project/index.js'
 
 const router = express.Router();
 
 
 // auth routes
-router.post('/auth/register/init' , Auth.register.init);
-router.post('/auth/register/verify' , Auth.register.verify);
-router.post('/auth/login' , Auth.Login)
+router.post('/auth/register/init', Auth.register.init);
+router.post('/auth/register/verify', Auth.register.verify);
+router.post('/auth/login', Auth.Login)
 
 
 // project route
-//router.route('/project').post(verifyJWT , Project.core.createProject)
+router.route(
+    '/project'
+)
+.post(verifyJWT, Project.core.createProject)
 
 export default router
