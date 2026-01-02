d0c25f12da2ac107acc0bb4d08f365760246fce6
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-02 21:30:15 +0530

    make authorizeFileAccess middleware for server file

---
+++ backend/src/middleware/authorizeFileAccess.middleware.js
@@ -0,0 +1,46 @@
+import rateLimit, { ipKeyGenerator } from "express-rate-limit";
+import { Model} from '../models/index.js'
+
+const memberLimit = rateLimit({
+  windowMs: 60 * 1000,
+  max: 400,
+  message: "Request bhaut zyada ho gaya",
+  keyGenerator: ipKeyGenerator,
+});
+
+const freeLimit = rateLimit({
+  windowMs: 60 * 1000,
+  max: 150,
+  message: "Request bhaut zyada ho gaya",
+  keyGenerator: ipKeyGenerator,
+});
+
+const authorizeFileAccess = async (req, res, next) => {
+    try {
+        const { filename } = req.params;
+
+        if (!filename) {
+            return res.status(400).json({
+                message: "filename missing"
+            })
+        }
+
+        const file = await Model.File.findOne({ filename }).populate('owner');
+        if (!file) {
+            return res.status(400).json({ message: "file not exist" });
+        }
+
+        const user = file.owner?.status;
+        const limit = user === 'member' ? memberLimit : freeLimit;
+        return limit(req, res, () => {
+            req.file = file;
+            next();
+        });
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "error While Server Your File" });
+    }
+};
+
+export { authorizeFileAccess };
