90825e03807446859bd99608cab0747a96929baf
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-02 21:31:27 +0530

    make getfile routes

--- backend/src/routes/publicUrl.routes.js
+++ backend/src/routes/publicUrl.routes.js
@@ -1,6 +1,12 @@
 import express from "express"
+import { authorizeFileAccess } from "../middleware/authorizeFileAccess.middleware.js";
+import { Project } from '../controllers/Project/index.js'
+
 
 const router = express.Router();
 
+router.get('/:projectid/:filename' , (req, res, next) => {
+    next();
+} , authorizeFileAccess, Project.file.get)
 
 export default router
