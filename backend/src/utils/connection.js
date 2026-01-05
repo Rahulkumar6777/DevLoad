75d692aa303336bd7787898eaa1b255bf7e9db30
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-05 13:37:58 +0530

    try to fix redis issue

--- backend/src/utils/connection.js
+++ backend/src/utils/connection.js
@@ -1,11 +1,7 @@
 import Redis from "ioredis";
 
 export const connection = new Redis("redis://redis:6379", {
-  maxRetriesPerRequest: 5,     
-  retryStrategy(times) {
-    if (times > 10) return null;
-    return Math.min(times * 500, 3000);
-  },
+  maxRetriesPerRequest: null,     
 });
 
 connection.on("ready", () => {
