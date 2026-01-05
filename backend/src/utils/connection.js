4f65432eaefa9b4000eebdd1dc49c4394b7b0b17
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-05 13:40:48 +0530

    try to fix redis issue

--- backend/src/utils/connection.js
+++ backend/src/utils/connection.js
@@ -1,13 +1,8 @@
-import Redis from "ioredis";
+import IORedis from 'ioredis';
 
-export const connection = new Redis("redis://redis:6379", {
-  maxRetriesPerRequest: null,     
-});
 
-connection.on("ready", () => {
-  console.log(" Redis READY (stable connection)");
-});
-
-connection.on("error", (err) => {
-  console.error(" Redis error:", err.message);
+export const connection = new IORedis({
+    maxRetriesPerRequest: null,
+    host: "redis",
+    port: 6379,
 });
