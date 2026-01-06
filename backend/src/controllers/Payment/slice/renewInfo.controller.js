03d6d5ab1cb891e2d940c33b766543be0d02938d
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-06 20:46:34 +0530

    make new controller

---
+++ backend/src/controllers/Payment/slice/renewInfo.controller.js
@@ -0,0 +1,28 @@
+import { Model } from "../../../models/index.js";
+
+export const renewInfo = async (req, res) => {
+    try {
+        const userId = req.user._id;
+
+        const previousOrder = await Model.CompletedOrder.findOne({userid: userId});
+        console.log(previousOrder)
+        if(!previousOrder){
+            return res.status(400).json({
+                message: "you are not eligible for renew"
+            })
+        }
+
+        const months = previousOrder.months
+
+        return res.status(200).json({
+            message: "Success",
+            month: months
+        })
+        
+
+    } catch (error) {
+        return res.status(500).json({
+            error: "Internal server Error"
+        })
+    }
+}
