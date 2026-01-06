80c67535bdb82a4aa997005dc2d8895a6763605b
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-06 20:45:39 +0530

    add

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,11 +1,11 @@
 import { Apikey } from "./slices/apikey.model.js";
+import { CompletedOrder } from "./slices/completedOrder.model.js";
 import { DeleteUserAccountModel } from "./slices/DeleteUserAccount.model.js";
 import { Domain } from "./slices/domain.model.js";
 import { File } from "./slices/file.model.js";
 import { OtpValidate } from "./slices/otpValidator.model.js";
 import { PendingOrder } from "./slices/PendingOrder.js";
 import { Project } from "./slices/projects.model.js";
-import { RenewSubscription } from "./slices/renewSubscription.model.js";
 import { TempUser } from "./slices/tempUser.model.js";
 import { User } from "./slices/user.Model.js";
 
@@ -19,5 +19,5 @@ export const Model = {
     File,
     Domain,
     PendingOrder,
-    RenewSubscription
+    CompletedOrder
 }
