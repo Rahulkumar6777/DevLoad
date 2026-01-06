91b3096f8b14f555de1d2ab776c8b5b2bea17f22
Rahulkumar6777 <rahulk48546@gmail.com>
2026-01-06 20:44:32 +0530

    make new model

---
+++ backend/src/models/slices/completedOrder.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const completedorderschema = new mongoose.Schema({
+    userid: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    oderid: {
+        type: String,
+        required: true
+    },
+    months: {
+        type: Number,
+        required: true
+    },
+    amount: {
+        type: Number
+    },
+    status: {
+        type: String,
+    }
+}, { timestamps: true})
+
+
+export const CompletedOrder = new mongoose.model('CompletedOrder', completedorderschema)
